283d9f53167d77a2a8260c984697be4b53ee0530
yuzhaosheng <614899753@qq.com>
2020-01-20 14:17:51 +0800

    init

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
-module play-mysqldirver
+module github.com/zhaosheng-yu/play-mysqldriver
+
+go 1.13
+
+require github.com/jinzhu/gorm v1.9.12 // indirect
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"math/rand"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/jinzhu/gorm"
+)
+
+func main() {
+	for i:=0;i<155;i++ {
+		gorm.Open("mysql", "root:pfdsj@(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=true&loc=Local")
+	}
+
+	db, err := gorm.Open("mysql", "root:pfdsj@(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=true&loc=Local")
+
+	if err != nil {
+		fmt.Println("open db error:%s", err)
+	}
+
+	defer db.Close()
+
+	user := struct{
+		ID int
+		Userid int
+		Name string
+	}{}
+
+	err = db.Table("user").First(&user).Error
+	if err != nil {
+		fmt.Println("query sql error:%s", err)
+	}
+
+	//batch insert
+	num := 10
+	rand.Seed(time.Now().UnixNano())
+	for i:=0;i<num;i++ {
+		uid := rand.Intn(num*10)
+		name := fmt.Sprintf("user%d",uid)
+		user := struct{
+			Userid int
+			Name string
+		}{uid,name}
+		err = db.Table("user").Create(&user).Error
+		if err != nil {
+			fmt.Println("create error:%s", err)
+			break
+		}
+	}
+
+	fmt.Println(user)
+
+	time.Sleep(time.Second*10)
+
+}
